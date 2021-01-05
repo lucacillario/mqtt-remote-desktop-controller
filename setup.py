92128b8310cf1834650beeeb0078671ecfd24ba4
Luca Cillario <luca@pop-os.localdomain>
2021-01-05 19:46:59 +0100

    Added volume and content control

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mqttrdc",
+    version="1.0",
+    author="Luca Cillario",
+    author_email="luca.cillario.95@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+)
